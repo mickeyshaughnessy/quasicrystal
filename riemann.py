b1282da5306e46632bf9e094ea512e55309f6751
Michael Shaughnessy <michaelshaughnessy@Michaels-MacBook-Pro.local>
2022-11-06 13:27:46 -0700

    update riemann

--- riemann.py
+++ riemann.py
@@ -7,14 +7,14 @@ def is_prime(n):
     # could also use int(sqrt(n))+1 as the upper limit:w
 
     for i in range(n, int(n/2)+1):
-        if n % i == 0:
+        if i % n == 0:
             return 0
     return 1
 
 def quick_pi(n):
     return sum([int(is_prime(i)) for i in range(n)]) 
 
-if name == "__main__":
+if __name__ == "__main__":
     print(quick_pi(1))
     print(quick_pi(1000))
     print(quick_pi(100000))
